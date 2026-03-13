d3ac141f5490bd6274746923db9f7a94612e364a
Nursultan Baktybekov <orenvady@gmail.com>
2026-03-13 21:49:58 +0600

    feat: implemented cli arguments parsing

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,32 @@
 // You should have received a copy of the GNU General Public License
 // along with this program. If not, see <https://www.gnu.org/licenses/>.
 
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Ignore case for text search
+    #[arg(short, long)]
+    ignore_case: bool,
+
+    /// Show line number
+    #[arg(short, long)]
+    line_number: bool,
+
+    /// Text to search in files
+    text_to_find: Option<Vec<String>>,
+}
+
 fn main() {
-    println!("rfast-find");
+    let args = Args::parse();
+    match args.text_to_find {
+        Some(ref text_to_find) => {
+            text_to_find.iter().for_each(|arg| print!("{} ", arg));
+            println!();
+        }
+        None => {
+            println!("No text to search is provided")
+        }
+    }
 }
